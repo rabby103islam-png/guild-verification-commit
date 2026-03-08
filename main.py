384411f2002759022aa3618d2629604cca133f23
rabby103islam-png <rabby103islam@gmail.com>
2026-03-08 13:22:42 +0600

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T07:22:47.868Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-08T07:22:50.531Z
+Commit: Patch API endpoint error handling
